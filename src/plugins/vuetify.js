0d60b128582bcd7b33253476919c16544138e0ff
sheryarAli <sheri.abhati@gmail.com>
2023-11-30 10:14:10 +0100

    Update vuetify.js
    custome theme code removed

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,25 +6,7 @@ import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
-const myCustomLightTheme = {
-    options: {
-        customProperties: true,
-    },
-    dark: false,
-    colors: {
-        background: () => url('https://c.animaapp.com/cuCqTOMO/img/untitled-2.png'),
-        // background: '#BEB8AF',
-        // surface: '#BEB8AF',
-        primary: '#6200EE',
-        'primary-darken-1': '#3700B3',
-        secondary: '#03DAC6',
-        'secondary-darken-1': '#018786',
-        error: '#B00020',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FB8C00',
-    },
-}
+
 
 export default createVuetify({
     theme: false,
